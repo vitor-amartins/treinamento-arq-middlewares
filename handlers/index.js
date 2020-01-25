6107de6fffe57a86628329be6eb067d0124b5c81
vitor-amartins <vmam@cin.ufpe.br>
2020-01-25 12:06:39 -0300

    Created the handlers

---
+++ handlers/index.js
@@ -0,0 +1,9 @@
+const requestHandler = require('request-handler');
+const errorHandler = require('error-handler');
+const notFoundHandler = require('not-found-handler');
+
+module.exports = {
+  requestHandler,
+  errorHandler,
+  notFoundHandler,
+};
