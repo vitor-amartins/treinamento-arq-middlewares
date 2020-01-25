baca889f162e6d12b6332923aff3041358a8ab12
vitor-amartins <vmam@cin.ufpe.br>
2020-01-25 11:41:26 -0300

    Added User model

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String, required: true, unique: true, trim: true,
+  },
+  password: {
+    type: String, required: true,
+  },
+  name: {
+    type: String, required: true, trim: true,
+  },
+}, {
+  timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
